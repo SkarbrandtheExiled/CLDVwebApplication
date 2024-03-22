27f9fcd750946f7e4e9b8368dde25769c0e02c7e
SkarbrandtheExiled <131450052+SkarbrandtheExiled@users.noreply.github.com>
2024-03-22 15:12:58 +0200

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using CLDVwebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLDVwebApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        public userTable usrtb = new userTable();
+
+    [HttpPost]
+
+    public ActionResult About(userTable Users)
+    {
+        var result = usrtb.insert_user(Users);
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet]
+    public IActionResult About() {
+        return View(usrtb);
+        }
+    }
+}
---
+++ Models/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CLDVwebApplication.Models
+{
+    public class userTable
+    {
+        public static string con_String = "Server=tcp:cldvdev.database.windows.net,1433;Initial Catalog=cldvdatabase.D8;Persist Security Info=False;User ID=ST10254164;Password=Knight72$2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        public static SqlConnection con = new SqlConnection(con_String);
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public int insert_user(userTable n)
+        {
+try
+            {
+                string sql = "INSERT INTO HomeController (userName, userSurname, userEmail) VALUES (@Name, @Surname, @Email)";
+
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Name", n.Name);
+                cmd.Parameters.AddWithValue("@Surname", n.Surname);
+                cmd.Parameters.AddWithValue("@Email", n.Email);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected;
+
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+        }
+    }
+}
